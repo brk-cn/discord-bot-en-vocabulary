9fadb5cc41a6adca5bb3f26a36a61bc418a9a33d
Burak Can <burakcanswe@gmail.com>
2024-05-11 15:21:12 +0300

    word quiz

--- bot/main.py
+++ bot/main.py
@@ -23,8 +23,26 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!'):
-        random_word = list(collection.aggregate([{ "$sample": { "size": 1 }}]))[0]
-        await message.channel.send(f"{random_word['word_english']} ({random_word['word_class']}) : {random_word['word_turkish']}")
+    if message.content == '!w':
+        rw = list(collection.aggregate([{ "$sample": { "size": 1 }}]))[0]
+        await message.channel.send(f"{rw['word_english']} ({rw['word_class']}) : {rw['word_turkish']}")
 
+    if message.content == '!q':
+        rw = list(collection.aggregate([{ "$sample": { "size": 1 }}]))[0]
+        await message.channel.send(f"{rw['word_english']} ({rw['word_class']})")
+
+        def check(msg):
+            return msg.author == message.author and msg.channel == message.channel
+
+        try:
+            response = await client.wait_for('message', timeout=30.0, check=check)
+        except asyncio.TimeoutError:
+            await message.channel.send('Time is up!')
+            return
+
+        if response.content.lower() == rw['word_turkish'].lower():
+            await message.channel.send('Correct!')
+        else:
+            await message.channel.send(f'Wrong! The correct answer is: {rw["word_turkish"]}')
+    
 client.run(TOKEN)
